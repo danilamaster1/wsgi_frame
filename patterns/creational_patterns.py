6b17ea0c5cb1ce12da3961559fb546715285ef85
Daniel <98400545+danilamaster1@users.noreply.github.com>
2023-02-27 01:32:58 +0300

    sys_patterns (#7)

--- patterns/creational_patterns.py
+++ patterns/creational_patterns.py
@@ -1,6 +1,9 @@
 from copy import deepcopy
 from quopri import decodestring
+from sqlite3 import connect
 from behavioral_patterns import Subject
+from architectural_system_pattern_unit_of_work import DomainObject
+from db_errors import DbCommitException, DbUpdateException, DbDeleteException, RecordNotFoundException
 
 
 class User:
@@ -12,7 +15,7 @@ class Teacher(User):
     pass
 
 
-class Student(User):
+class Student(User, DomainObject):
     def __init__(self, name):
         self.courses = []
         super().__init__(name)
@@ -130,3 +133,74 @@ class Engine:
         val_b = bytes(val.replace('%', '=').replace('+', ' '), 'UTF_8')
         val_decode_str = decodestring(val_b)
         return val_decode_str.decode('UTF-8')
+
+
+class StudentMapper:
+
+    def __init__(self, connection):
+        self.connection = connection
+        self.cursor = connection.cursor()
+        self.tablename = 'student'
+
+    def all(self):
+        statement = f'SELECT * FROM {self.tablename}'
+        self.cursor.execute(statement)
+        result = []
+        for item in self.cursor.fetchall():
+            id, name = item
+            student = Student(name)
+            student.id = id
+            result.append(student)
+        return result
+
+    def find_by_id(self, id):
+        statment = f'SELECT id, name FROM {self.tablename} WHERE id=?'
+        self.cursor.execute(statment, (id, ))
+        result = self.cursor.fetchone()
+        if result:
+            return Student(*result)
+        else:
+            raise RecordNotFoundException(f'record with id={id} not found')
+
+    def insert(self, obj):
+        statement = f'INSERT INTO {self.tablename} (name) VALUES (?)'
+        self.cursor.execute(statement, (obj.name,))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbCommitException(e.args)
+
+    def update(self, obj):
+        statement = f'UPDATE {self.tablename} SET name=? WHERE id=?'
+
+        self.cursor.execute(statement, (obj.name, obj.id))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbUpdateException(e.args)
+
+    def delete(self, obj):
+        statement = f'DELETE FROM {self.tablename} WHERE id=?'
+        self.cursor.execute(statement, (obj.id, ))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbDeleteException(e.args)
+
+
+connection = connect('patterns.sqlite')
+
+
+class MapperRegistry:
+    mappers = {
+        'student': StudentMapper,
+    }
+
+    @staticmethod
+    def get_mapper(obj):
+        if isinstance(obj, Student):
+            return StudentMapper(connection)
+
+    @staticmethod
+    def get_current_mapper(name):
+        return MapperRegistry.mappers[name](connection)
